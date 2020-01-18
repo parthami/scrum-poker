6017998a4531222353897004d7aa4d3b6834f910
parthami <pac442@bham.ac.uk>
2020-01-18 14:42:48 +0000

    Added Bulma and basic form to estimate tickets

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <!DOCTYPE html>
+  <div id="app">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <PokerForm></PokerForm>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+import PokerForm from './components/PokerForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PokerForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
