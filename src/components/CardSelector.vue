49436321debe113c6e0739d1c9b25b0478b146a5
Parth Chandratreya <chandratreya.parth@gmail.com>
2020-01-18 14:42:48 +0000

    Added Bulma and basic form to estimate tickets

---
+++ src/components/CardSelector.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="section">
+    <h1 class="subtitle is-3">{{ title }}</h1>
+    <div class="columns is-desktop is-multiline is-centered">
+    <p class="column is-1" v-for="cardValue in cardValues" v-bind:key="cardValue">
+        <button v-on:click="clicked(cardValue)" class="button">{{cardValue}}</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['title'],
+    data() {
+        return {
+            cardValues: ['?', '0', '1', '2', '3', '5', '8' ,'13']
+        }
+    },
+    methods: {
+        clicked(cardValue) {
+            this.$emit('input', cardValue);
+        }
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/PokerForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-two-thirds">
+        <div class="box">
+          <div class="field">
+            <label class="title">Ticket</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Text input" v-model="name" />
+            </div>
+          </div>
+          <CardSelector title="Story Points" v-model="storyPoints"></CardSelector>
+          <br />
+          <CardSelector title="Time Estimate" v-model="timeEstimate"></CardSelector>
+          <br />
+          <button v-on:click="saveEstimations" class="button is-success">Submit</button>
+        </div>
+      </div>
+      <div class="column">
+        <div class="box">
+          <ul class="list">
+          <li class="list-item" v-for="item in list" v-bind:key="item">
+            {{ item[0] }} - {{item[1]}} / {{item[2]}}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardSelector from "./CardSelector";
+
+export default {
+  data() {
+    return {
+      list: [],
+      name: "",
+      storyPoints: "",
+      timeEstimate: ""
+    };
+  },
+  components: {
+    CardSelector
+  },
+  methods: {
+    saveEstimations() {
+      this.list.push([this.name, this.storyPoints, this.timeEstimate]);
+      // console.log(this.list);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
