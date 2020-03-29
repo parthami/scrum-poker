fb92f2fd0c23a63b6854461b6ed7777061831dd2
parthami <pac442@bham.ac.uk>
2020-03-29 21:39:36 +0100

    added navbar and ticket popup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar is-fixed-top is-dark" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item">
+     <span>Scrum Poker</span>  
+    </a>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
