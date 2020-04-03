448712a250ad269d9fdb52b01f305ace6ca25ab5
parthami <pac442@bham.ac.uk>
2020-04-03 14:19:31 +0100

    added new SPA components

---
+++ src/components/CreateRoom.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="field">
+      <label class="label">Room name</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Platform Team" v-model="name" />
+      </div>
+    </div>
+    <div class="field">
+      <div class="control">
+        <label class="checkbox">
+          <input type="checkbox" />
+          Lock room?
+        </label>
+      </div>
+    </div>
+      <div class="field">
+        <div class="control">
+          <button class="button is-link" @click="createRoom()">Submit</button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import { store } from "../store.js";
+
+export default {
+    data() {
+        return {
+            name: ''
+        };
+    },
+    methods: {
+        createRoom() {
+            store.createRoom(this.name);
+            this.$router.push('room');
+        }
+    },
+};
+</script>
+
+<style scoped>
+.container {
+  padding-top: 2rem;
+}
+</style>
