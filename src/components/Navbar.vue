d8855a1c2f8d99c858c8f7ad43a79ad67b1ab383
parthami <pac442@bham.ac.uk>
2020-04-03 14:21:19 +0100

    updated nav bar for vue router and disabled ticket popup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,47 @@
 <template>
   <nav class="navbar is-fixed-top is-dark" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item">
-     <span>Scrum Poker</span>  
-    </a>
-  </div>
-</nav>
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <span>Scrum Poker</span>
+      </a>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong @click="signIn()">{{ uid }}</strong>
+          </a>
+          <router-link to="/create">
+            <a class="button is-primary">
+              <strong>Create Room</strong>
+            </a>
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <div></div>
+  </nav>
 </template>
 
 <script>
-export default {
+import firebase from "firebase/app";
+import "firebase/auth";
+import { store } from "../store.js";
 
-}
+export default {
+  data() {
+    return {
+      uid: "Sign in"
+    };
+  },
+  methods: {
+    signIn() {
+      firebase.auth().signInAnonymously();
+      this.uid = store.currentUser.uid;
+    },
+  }
+};
 </script>
 
 <style>
-
 </style>
--- src/components/TicketPopup.vue
+++ src/components/TicketPopup.vue
@@ -22,7 +22,7 @@ export default {
   data() {
     return {
       rawTickets: "",
-      modalActive: true
+      modalActive: false
     };
   },
   methods: {
