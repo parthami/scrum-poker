75bf52fd6758b2c441c13b7d8d68a35dd01733a0
parthami <chandratreya.parth@gmail.com>
2020-05-23 23:16:44 +0100

    404 route and component

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,25 @@
+<template>
+<body>
+  <section class="section">
+    <div class="columns">
+      <div class="column column is-half is-offset-one-quarter">
+        <p>404</p>
+      </div>
+    </div>
+  </section>
+</body>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@800&display=swap');
+
+p {
+    font-family: 'Inconsolata', monospace;
+    text-align: center;
+    font-size: 6rem;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import CreateRoom from './components/CreateRoom.vue';
 import Home from './components/Home.vue';
 import Dashboard from './components/Dashboard.vue';
 import SignInUp from './components/SignInUpComponent.vue';
+import NotFound from './components/NotFoundComponent.vue';
 
 export const routes = [
     { name: 'home', path: '/', component: Home },
     { name: 'create', path: '/create', component: CreateRoom },
-    { name: 'room', path: '/room/:id', component: PokerForm, meta: {auth : true} },
+    { name: 'room', path: '/room/:id', component: PokerForm, meta: { auth: true } },
     { name: 'visited', path: '/dashboard', component: Dashboard },
     { name: 'signInUp', path: '/signin', component: SignInUp, props: (route) => ({ room: route.query.room }) },
+    { path: '*', component: NotFound },
 ];
